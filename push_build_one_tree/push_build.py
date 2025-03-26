3e45177e260d1cb0f03a67651bc5aeae6785cb1e
Kalmuk Yaropolk <yarko.kalmuk@gmail.com>
2025-03-26 22:52:43 +0200

    done 3 task

--- push_build_one_tree/push_build.py
+++ push_build_one_tree/push_build.py
@@ -1,15 +1,19 @@
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
 
 
 def push(head, data):
-    return Node(data, head)
+    new_node = Node(data)
+    new_node.next = head
+    return new_node
 
 
 def build_one_two_three():
-    head = None
+    link = None
     for node in range(3, 0, -1):
-        head = Node(node, head)
+        head = Node(node)
+        head.next, link = link, head
     return head
+print(build_one_two_three())
